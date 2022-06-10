f41cedee4de8486ea2c8e6b4c7061dfb57970d23
maryannochkaa <marianna270603@gmail.com>
2022-06-11 00:24:46 +0200

    add: registration

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-// const mysql = require('mysql2');
+const mysql = require('mysql');
 const session = require('express-session');
-const config = require('./config.json');
+const config = require('../config.json');
+const db = require('./models/db');
 
 let app = express();
 
@@ -20,12 +21,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function (request, response) {
-	response.sendFile(path.join(__dirname + '/views/login.html'));
+	response.sendFile(path.join(__dirname + '/views/registration.html'));
 });
 
 app.get('/public/styles.css', function (request, response) {
 	response.sendFile(path.join(__dirname + '/public/styles.css'));
 });
+
+app.get('/public/assets/logo.png', function (request, response) {
+	response.sendFile(path.join(__dirname + '/public/assets/logo.png'));
+});
+
+////////////////
+
+app.post('/auth', function (request, response) {
+	const { email, login, password } = request.body;
+
+	if (!request.body)
+		return response.sendStatus(400);
+
+	if (email && login && password) {
+		db.query('SELECT * FROM users WHERE email = ? AND login = ? AND password = ?', [email, login, password], function (error, results, fields) {
+			if (error) throw error;
+
+			if(results.lenght > 0) {
+				request.flash('error', 'The email is already in use');
+				response.redirect('/')
+			}
+		});
+		response.end();
+	}
+});
+
+////////////////
 
 app.listen(port, hostname, () => {
 	console.log(`Server is running at http://${hostname}:${port}/`);
--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql2');
-const config = require('./config.json');
+const mysql = require('mysql');
+const config = require('../config.json');
 const sql = `SELECT * FROM users`;
 
-const dbConnection = mysql.createConnection(config);
+const db = mysql.createConnection(config);
 
-dbConnection.connect((err) => {
+db.connect((err) => {
     if (err) { throw err; }
     console.log("DB is successfully connected!");
 });
 
-dbConnection.query(sql, (err, result) => {
+db.query(sql, (err, result) => {
     if (err) console.log(err);
     console.log(result);
 });
 
-module.exports = dbConnection;
+module.exports = db;
